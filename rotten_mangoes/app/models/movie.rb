c4f4eb49629c4e79bfa9fc10896ba6b0f81193f1
theabuitendyk <theabuitendyk@users.noreply.github.com>
2015-07-16 19:12:42 +0000

    adding movie validations

--- rotten_mangoes/app/models/movie.rb
+++ rotten_mangoes/app/models/movie.rb
@@ -1,2 +1,19 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, :director, :description, :poster_image_url, :release_date,
+    presence: true
+
+  validates :runtime_in_minutes,
+    numericality: { only_integer: true }
+
+  validate :release_date_is_in_the_future
+
+  protected
+
+  def release_date_is_in_the_future
+    if release_date.present?
+      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
+    end
+  end
+
 end
